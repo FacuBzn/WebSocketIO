5c20ae419829299f16050a837baf22b8fb72e94a
facubazan <juanfacundobazan@gmail.com>
2022-08-24 08:40:55 -0300

    mensajes de conexion y desconexion para proyecto de websocket.io

---
+++ public/js/socket-client.js
@@ -0,0 +1,19 @@
+//Referencias del html
+const lblonline = document.querySelector('#lblonline');
+const lbloffline = document.querySelector('#lbloffline');
+
+
+const socket = io();
+
+socket.on('connect', ()=>{
+    console.log('conectado');
+
+    lbloffline.style.display = 'none';
+    lblonline.style.display = '';
+});
+
+socket.on('disconnect', ()=>{
+    console.log('desconectado del servidor');
+    lbloffline.style.display = '';
+    lblonline.style.display = 'none';
+});
